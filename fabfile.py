151ff914d40225cd964d1b5a2499da130a3c8cd2
akshay bhat <akshayubhat@gmail.com>
2015-12-26 06:13:44 -0500

    feature extraction using tensorflow inception model

--- fabfile.py
+++ fabfile.py
@@ -1,13 +1,226 @@
 import os,sys,logging,time
-from fabric.api import env,local,run,sudo,put,cd,lcd,puts,task,get
+from fabric.api import env,local,run,sudo,put,cd,lcd,puts,task,get,hide
 from fabric.operations import local as lrun, run
 from fabric.state import env
 from settings import BUCKET_NAME
+import data
 from settings import USER,private_key,HOST
 env.user = USER
 env.key_filename = private_key
 env.hosts = [HOST,]
 
+SKIP = """
+adrijana-dejanovic
+agnes-buzala
+agnes-sokolowska
+agyness-deyn
+ai-tominaga
+alana-bunte
+alessandra-albrecht
+alexandra-agoston
+alexandra-spencer
+alexa-yudina
+alice-rausch
+alina-baikova
+aline-weber
+alison-nix
+ali-stephens
+alize-barange
+alla-kostromichova
+allyson-ally-ertel
+amanda-ware
+anais-mali
+andressa-fontana
+andzelika-buivydaite
+aneta-pajak
+anja-leuenberger
+anna-arendshorst
+anna-grostina
+anna-jagodzinska
+anna-piirainen
+annemarije-rus
+annemijn-dijs
+astrid-holler
+athena-wilson
+auguste-abeliunaite
+bambi-northwood--blyth
+brenda-kranz
+brynja-j%C3%B3nbjarnard%C3%B3ttir
+caitlin-lomax
+camille-rowe
+candice-swanepoel
+carolina-thaler
+caroline-brasch-nielsen
+chloe-norgaard
+claire-granlund
+clarine-de-jonge
+constance-jablonski
+crystal-noreiga
+dafne-cejas
+dalianah-arekion
+daria-pilnitskaya
+daria-werbowy
+darla-baker
+deimante-misiunaite
+diana-dondoe
+diana-gartner
+diana-moldovan
+drielly-oliveira
+eleonore-toulin
+elettra-rossellini-wiedemann
+elianne-smit
+elise-crombez
+eliza-sys
+ellen-danes
+elsa-sylvan
+elyse-saunders
+emilia-nilsson
+emma-maclaren
+enly-tammela
+ernesta-petkeviciute
+fanny-francois
+georgia-may-jagger
+georgie-wass
+grace-hartzel
+hanna-verhees
+hanne-gaby-odiele
+hedvig-palm
+heidi-mount
+heidi-verster
+helena-magone
+henrietta-hellberg
+ieva-birzina
+ilze-bajare
+ines-crnokrak
+inga-eiriksdottir
+ioanna-ntenti
+isabeli-fontana
+isabella-farrell
+isabella-melo
+iveta-bogdane
+iza-olak
+jada-joyce
+jamilla-hoogenboom
+jana-wieland
+jeanne-cadieu
+jeisa-chiminazzo
+jemma-baines
+jenny-sweeney
+jessiann-gravel-beland
+jiaye-wu
+joan-smalls
+johanna-wahlberg
+jules-mordovets
+julia-johansen
+katarina-ivanovska
+katarina-ivanovska/.DS_Store
+kate-ellery
+kate-king
+kate-kosushkina
+katerina-netolicka
+katryn-kruger
+kat-shandruk
+katy-dron
+kelie-santos
+kelley-ash
+kelly-mittendorf
+kerstin-mannik
+kim-noorda
+kirstin-kragh-liljegren
+kristina-tsirekidze
+kristina-tsvetkova
+kristine-zandmane
+laima-andersone
+lara-mullen
+lara-stone_ii
+laura-julie
+lauren-rippingham
+lia-pavlova
+lien-vieira
+liliana-dominguez
+lily-zhi
+linda-vojtova
+lin-kjerulf
+list.txt
+lone-praesto
+louise-pedersen
+lucie-von-alten
+mackenzie-drazan
+maddie-kulicka
+maddison-brown
+madison-headrick
+magda-laguinge
+maggie-laine
+maria-borges_iii
+maria-bradley
+mariacarla-boscono
+maria-palm
+marihenny-rivera
+mariya-markina
+marthe-van-stuijvenberg
+megan-mcnierney
+meghan-collison
+melodie-monrose
+michelle-van-bijnen
+michelle-zwaal
+mila-krasnoiarova
+mila-mihaljcic
+mimmi-soderstrom
+monika-sawicka
+montana-cox
+naemi-schink
+nastya-zhidkikh
+natalia-semanova
+noa-vermeer
+noelle-roques
+noreen-carmody
+nova-malanova
+nyasha-matonhodze
+olga-maliouk
+ophelie-rupp
+patrycja-gardygajlo
+paulina-heiler
+querelle-jansen
+ranya-mordanova
+romee-strijd
+romee-strijd/romee_strijd_8053510.jpg
+romina-lanaro
+romina-lanaro/3057948.jpg
+roos-van-bosstraeten
+rose-cordero
+ros-georgiou
+ruby-jean-wilson
+sadie-newman
+sanne-nijhof
+sasha-antonowskaia
+sasha-pivovarova
+saskia-de-brauw
+serafima-kobzeva
+sonya-gorelova
+sophie-srej
+sophie-touchet
+stella-tennant
+stina-rapp-wastenson
+sui-he
+suzie-bird
+taja-feistner
+tania-balash%21
+thairine-garcia
+thaisa-liz
+ubah-hassan
+ugne-andrikonyte
+vala-thorsteinsson
+valentina-zelyaeva
+valeriane-le-moi
+vanessa-moody
+veneda-budny
+vita-sidorkina
+waleska-gorczevski
+xiao-wen-ju
+yana-karpova
+yana-van-ginneken
+zuzanna-stankiewicz
+"""
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
@@ -106,6 +319,41 @@ def upload():
             put("{}".format(d),"workspace/")
 
 
-"""
-mogrify -resize '500x500' *.jpg
-"""
+@task
+def preprocess():
+    # sudo("apt-get install -y parallel")
+    # run("mkdir images")
+    # with cd("images"):
+    #     for line in SKIP.split("\n"):
+    #         if line.strip():
+    #             run("mkdir {}".format(line.strip()))
+    dataset = data.Dataset()
+    for count,name in dataset.sorted():
+        skip = False
+        logging.info("starting {} {}".format(name,count))
+        with cd("images"):
+            try:
+                run("mkdir {}".format(name))
+            except:
+                skip = True
+                logging.info("skipping {}".format(name))
+            if not skip:
+                with cd("{}".format(name)):
+                    logging.info("downloading {}".format(name))
+                    with hide('output'):
+                        run("aws s3 cp s3://aub3data/models/{} . --recursive --region \"us-east-1\"".format(name))
+                    logging.info("renaming {}".format(name))
+                    run('for f in * ; do mv "$f" "{}_$f" ; done'.format(name.replace('-','_')))
+                    logging.info("resizing {}".format(name))
+                    try:
+                        with hide('output'):
+                            run('find -iname "*.jpg" -type f -print0 | parallel --progress -0 -j +0 "mogrify -resize \'500x500\'"')
+                        logging.info("finished {}".format(name))
+                    except:
+                        logging.info("Error while resizing {}".format(name))
+                        pass
+                    logging.info("Uploading started {}".format(name))
+                    with hide('output'):
+                        run('aws s3 mv ../{} s3://aub3data/dataset/ --recursive --region "us-east-1"'.format(name)) # run("find . -iname '*.jpg' -exec mv --target-directory ../dataset/ {} +")
+                    logging.info("Uploading finished {}".format(name))
+
---
+++ inceptionv3.py
@@ -0,0 +1,75 @@
+import re
+import numpy as np
+import tensorflow as tf
+import time,glob
+from tensorflow.python.platform import gfile
+
+class NodeLookup(object):
+    def __init__(self):
+        label_lookup_path = 'data/imagenet_2012_challenge_label_map_proto.pbtxt'
+        uid_lookup_path = 'data/imagenet_synset_to_human_label_map.txt'
+        self.node_lookup = self.load(label_lookup_path, uid_lookup_path)
+
+    def load(self, label_lookup_path, uid_lookup_path):
+        proto_as_ascii_lines = gfile.GFile(uid_lookup_path).readlines()
+        uid_to_human = {}
+        p = re.compile(r'[n\d]*[ \S,]*')
+        for line in proto_as_ascii_lines:
+            parsed_items = p.findall(line)
+            uid = parsed_items[0]
+            human_string = parsed_items[2]
+            uid_to_human[uid] = human_string
+        node_id_to_uid = {}
+        proto_as_ascii = gfile.GFile(label_lookup_path).readlines()
+        for line in proto_as_ascii:
+            if line.startswith('  target_class:'):
+                target_class = int(line.split(': ')[1])
+            if line.startswith('  target_class_string:'):
+                target_class_string = line.split(': ')[1]
+                node_id_to_uid[target_class] = target_class_string[1:-2]
+        node_id_to_name = {}
+        for key, val in node_id_to_uid.items():
+            if val not in uid_to_human:
+                tf.logging.fatal('Failed to locate: %s', val)
+            name = uid_to_human[val]
+            node_id_to_name[key] = name
+        return node_id_to_name
+
+    def id_to_string(self, node_id):
+        if node_id not in self.node_lookup:
+            return ''
+        return self.node_lookup[node_id]
+
+
+
+
+with gfile.FastGFile('network.pb', 'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    _ = tf.import_graph_def(graph_def, name='')
+
+
+if __name__ == '__main__':
+    image_data = []
+    for i,fname in enumerate(glob.glob("dataset*/*.jpg")):
+        image_data.append(gfile.FastGFile(fname, 'rb').read())
+        if i == 100:
+            break
+    print "{} Images loaded".format(i)
+    with tf.Session() as sess:
+        node_lookup = NodeLookup()
+        for data in image_data:
+            start = time.time()
+            try:
+                softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+                predictions = sess.run(softmax_tensor,{'DecodeJpeg/contents:0': data})
+                predictions = np.squeeze(predictions)
+            except:
+                print "error"
+                pass
+            print time.time()-start
+            # top_k = predictions.argsort()[-5:][::-1]
+            # for node_id in top_k:
+            #     human_string = node_lookup.id_to_string(node_id)
+            #     score = predictions[node_id]
+            #     print('%s (score = %.5f)' % (human_string, score))
